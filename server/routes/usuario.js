0544148327936c2b116de3c4d188f58f208cbedb
Fernando.Zamacona <fzamacona14@gmail.com>
2020-12-04 20:16:53 -0600

    Creada la cadena de conexion con mongo atlas en la nube

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,47 +1,48 @@
 const express = require('express');
-const _ = require("underscore");
-const Usuario = require("../models/usuario");
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
 const app = express();
 
-  app.get('/usuario', function (req, res) {
-      let desde = req.query.desde || 0; 
-      let: hasta = req.query.hasta || 5;
+app.get('/usuario', function(req, res) {
+    let desde = req.query.desde || 0;
+    let hasta = req.query.hasta || 5;
 
-      Usuario.find({ estado: true })
-      .skip(Number(desde))
-      .limit(Number(hasta))
-      .exec((err, usuarios) => {
-         if(err) {
-             return res.status(400).json({
-                 ok: false,
-                 msg: "ocurrio un error al momento de consultar",
-                 err
-             });
-         }
+    Usuario.find({ estado: true })
+        .skip(Number(desde))
+        .limit(Number(hasta))
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'Ocurrio un error al momento de consultar',
+                    err
+                });
+            }
 
-         res.json({
-             ok: true,
-             msg: "lista de usuarios obtenida con exito",
-             conteo: usuarios.length,
-             usuarios
-         });
-      });
-  });
-  
-  app.post('/usuario', function(req, res) {
+            res.json({
+                ok: true,
+                msg: 'Lista de usuarios obtenida con exito',
+                conteo: usuarios.length,
+                usuarios
+            });
+        });
+});
+
+app.post('/usuario', function(req, res) {
     let body = req.body;
     let usr = new Usuario({
         nombre: body.nombre,
         email: body.email,
-        password: body.password
+        password: bcrypt.hashSync(body.password, 10)
     });
 
     usr.save((err, usrDB) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
                 ok: false,
-                msg: "ocurrio un error el cielo se cae",
-                err 
+                msg: 'Ocurrio un error',
+                err
             });
         }
 
@@ -53,30 +54,29 @@ const app = express();
     });
 });
 
-  
-  app.put('/usuario/:id', function (req, res) {
-      let id = req.params.id;
-      let body = _.pick(req.body, ["nombre", "email"]);
-      Usuario.findByIdAndUpdate(id, body, 
-        { new: true, runValidators: true, context: "query" }, 
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' },
         (err, usrDB) => {
-            if(err) {
+            if (err) {
                 return res.status(400).json({
                     ok: false,
-                    msg: "ocurrio un error al momento de actualizar",
-                    err 
+                    msg: 'Ocurrio un error al momento de actualizar',
+                    err
                 });
             }
 
             res.json({
                 ok: true,
-                msg: "usuarioactualizado con exito",
-                usrDB 
+                msg: 'Usuario actualizado con exito',
+                usuario: usrDB
             });
-    }); 
-  }); 
-  
-  app.delete('/usuario/:id', function(req, res) {
+        });
+});
+
+app.delete('/usuario/:id', function(req, res) {
     // let id = req.params.id;
 
     // Usuario.deleteOne({ _id: id }, (err, usuarioBorrado) => {
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require ("./config/config")
+require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -6,28 +6,28 @@ const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
- 
+
 // parse application/json
 app.use(bodyParser.json());
- 
-app.get('/', function (req, res) {
-    res.send('<h1> bienvenido a mi servidor REST </h1>');
-  });
 
-  app.use(require("./routes/usuario"));
-  app.use(require('./routes/categoria'));
+app.get('/', function(req, res) {
+    res.send('<h1>Bienvenido a mi servidor REST</h1>');
+});
 
-mongoose.connect('mongodb://localhost:27017/cafeteria', {
+app.use(require('./routes/usuario'));
+app.use(require('./routes/categoria'));
+app.use(require('./routes/login'));
+
+mongoose.connect('mongodb+srv://admin:1yVZWef1GJE1KUcE@cluster0.vxzbd.mongodb.net/cafeteria', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-  
 }, (err, res) => {
-    if(err) throw err;
-    console.log('Base de datos ONLINE')
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
- 
-app.listen(process.env.PORT, () =>{
-    console.log ("el servidor esta en linea por el puerto" , process.env.PORT);
+
+app.listen(process.env.PORT, () => {
+    console.log('El servidor esta en linea por el puerto ', process.env.PORT);
 });
